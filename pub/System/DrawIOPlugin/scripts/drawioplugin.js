bc55e80fcfca564131c074325ff82baf8e320de3
Daniel O'Connor <Daniel.O'Connor@schneider-electric.com>
2018-06-17 00:42:34 +0930

    Initial commit, basics work - can edit and save an image. ToDo  - work out how strikeone validation works  - remove debugging

---
+++ pub/System/DrawIOPlugin/scripts/drawioplugin.js
@@ -0,0 +1,65 @@
+$(function() {
+  document.addEventListener('dblclick', function(evt) {
+    var url = 'https://www.draw.io/?embed=1&ui=atlas&spin=1&modified=unsavedChanges&proto=json';
+    var source = evt.srcElement || evt.target;
+
+    if (source.nodeName == 'IMG' && source.className == 'drawio') {
+      if (source.drawIoWindow == null || source.drawIoWindow.closed) {
+	// Implements protocol for loading and exporting with embedded XML
+	var receive = function(evt) {
+	  if (evt.data.length > 0 && evt.source == source.drawIoWindow) {
+	    var msg = JSON.parse(evt.data);
+
+	    // Received if the editor is ready
+	    if (msg.event == 'init') {
+	      // Sends the data URI with embedded XML to editor
+	      source.drawIoWindow.postMessage(JSON.stringify({action: 'load', xml: source.getAttribute('src')}), '*');
+	    }
+	    // Received if the user clicks save
+	    else if (msg.event == 'save') {
+	      console.log('saving');
+	      // Sends a request to export the diagram as XML with embedded SVG
+	      source.drawIoWindow.postMessage(JSON.stringify(
+		{action: 'export', format: 'xmlsvg', spinKey: 'saving'}), '*');
+	    }
+	    // Received if the export request was processed
+	    else if (msg.event == 'export') {
+	      console.log('exporting');
+	      var params = {
+		validation_key : source.getAttribute('validation-key'),
+		filename : source.getAttribute('filename'),
+		_web : foswiki.getPreference('WEB'),
+		_topic : foswiki.getPreference('TOPIC'),
+		data : msg.data,
+	      };
+	      $.post(foswiki.getScriptUrl('rest', 'DrawIOPlugin', 'upload'), params).done(function() {
+		alert('upload done');
+	      }).fail(function() {
+		alert('failed to upload');
+	      });
+	      // Updates the data URI of the image
+	      source.setAttribute('src', msg.data);
+	    }
+
+	    // Received if the user clicks exit or after export
+	    if (msg.event == 'exit' || msg.event == 'export') {
+	      console.log('exiting');
+	      // Closes the editor
+	      window.removeEventListener('message', receive);
+	      source.drawIoWindow.close();
+	      source.drawIoWindow = null;
+	    }
+	  }
+	};
+	// Opens the editor
+	window.addEventListener('message', receive);
+	source.drawIoWindow = window.open(url);
+      }
+      else
+      {
+	// Shows existing editor window
+	source.drawIoWindow.focus();
+      }
+    }
+  });
+});
