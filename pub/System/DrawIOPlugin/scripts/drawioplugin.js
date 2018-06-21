5e874e62c8ab681b0e9ee5f8cbc9dc3948253e75
Daniel O'Connor <Daniel.O'Connor@schneider-electric.com>
2018-06-21 22:29:02 +0930

    Refactor code into a few functions so it's less of a mess.

--- pub/System/DrawIOPlugin/scripts/drawioplugin.js
+++ pub/System/DrawIOPlugin/scripts/drawioplugin.js
@@ -1,6 +1,5 @@
 (function($) {
-  document.addEventListener('dblclick', function(evt) {
-    var url = 'https://www.draw.io/?embed=1&ui=atlas&spin=1&modified=unsavedChanges&proto=json';
+  function evthandler(evt) {
     var source = evt.srcElement || evt.target;
 
     if (source.nodeName == 'IMG' && source.className == 'drawio') {
@@ -9,62 +8,47 @@
 	var receive = function(evt) {
 	  if (evt.data.length > 0 && evt.source == source.drawIoWindow) {
 	    var msg = JSON.parse(evt.data);
-
+	    var srcurl = source.getAttribute('src');
+	    var filename = source.getAttribute('filename');
 	    // Received if the editor is ready
 	    if (msg.event == 'init') {
 	      // Fetch image, convert to a data URI and pass to the editor
-	      var dataurl = source.getAttribute('src');
 	      // hard won knowledge from http://stackoverflow.com/questions/20035615/using-raw-image-data-from-ajax-request-for-data-uri
 	      // via David Benson <davidjgraph@gmail.com>
 	      var xmlHTTP = new XMLHttpRequest();
-	      xmlHTTP.open('GET', dataurl, true);
+	      xmlHTTP.open('GET', srcurl, true);
 	      xmlHTTP.responseType = 'arraybuffer';
 	      xmlHTTP.onload = function(e) {
-		var arr = new Uint8Array(this.response);
-		var raw = String.fromCharCode.apply(null, arr);
-		var b64 = btoa(raw);
-		var dataURI = "data:image/svg+xml;base64," + b64;
+		var dataURI = "data:image/svg+xml;base64," + data2b64(this.response);
+
 		source.drawIoWindow.postMessage(JSON.stringify({action: 'load', xml: dataURI}), '*');
 	      };
 	      xmlHTTP.send();
 	    }
 	    // Received if the user clicks save
 	    else if (msg.event == 'save') {
-	      // Sends a request to export the diagram as SVG with embedded XML
+	      // Sends request to export the diagram as SVG with embedded XML
 	      source.drawIoWindow.postMessage(JSON.stringify(
 		{action: 'export', format: 'xmlsvg', spinKey: 'saving'}), '*');
 	    }
 	    // Received if the export request was processed
 	    else if (msg.event == 'export') {
 	      // Construct form to upload file
-	      var filename = source.getAttribute('filename');
 	      var formData = new FormData();
 	      formData.append('noredirect', 1);
 	      if (typeof(StrikeOne) !== 'undefined') {
-		var key = source.getAttribute('data-validation-key');
-		var key1 = StrikeOne.calculateNewKey(key);
-		formData.append('validation_key', key1);
+		var key = StrikeOne.calculateNewKey(source.getAttribute('data-validation-key'));
+		formData.append('validation_key', key);
 	      }
-	      // Decode file data from Draw.IO
-	      var byteString;
-	      if (msg.data.split(',')[0].indexOf('base64') >= 0)
-		byteString = atob(msg.data.split(',')[1]);
-	      else
-		byteString = unescape(msg.data.split(',')[1]);
-
-	      // separate out the mime component
-	      var mimeString = msg.data.split(',')[0].split(':')[1].split(';')[0];
-
-	      // Create blob to hold data
-	      var file = new Blob([byteString], {type: mimeString});
-	      formData.append('filepath', file, filename);
+	      var blob = decodeURI(msg.data);
+	      formData.append('filepath', blob, filename);
 
 	      var url = foswiki.getScriptUrl(
                 'upload',
                 foswiki.getPreference('WEB'),
                 foswiki.getPreference('TOPIC'));
 
-	      // Actually to the upload
+	      // Actually do the upload
               $.post({
 		url : url,
 		data: formData,
@@ -73,29 +57,13 @@
                 processData: false,
                 cache: false
 	      }).done(function(data, textStatus, jqXHR) {
-		// Force existing image to be reloaded
-		source.setAttribute('src', source.getAttribute('src') + '?' + new Date().getTime());
-		// Extract new validation key and update our copy
+		// Upload worked, refresh existing image
+		source.setAttribute('src', srcurl + '?' + new Date().getTime());
+		// Extract new validation key and update our copy for a subsequent edit
 		source.setAttribute('data-validation-key', '?' + jqXHR.getResponseHeader('x-foswiki-validation'));
 	      }).fail(function(jqXHR, textStatus, errorThrown) {
-		// Failed to upload, get the browser to save the file to rescue user effort.
-		// https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
-		// https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
-		if (window.navigator.msSaveOrOpenBlob) // IE10+
-		  window.navigator.msSaveOrOpenBlob(file, source.getAttribute('filename'));
-		else { // Others
-		  var a = document.createElement("a"),
-                      url = URL.createObjectURL(file);
-		  a.href = url;
-		  a.download = source.getAttribute('filename');
-		  document.body.appendChild(a);
-		  a.click();
-		  setTimeout(function() {
-		    document.body.removeChild(a);
-		    window.URL.revokeObjectURL(url);
-		  }, 0);
-		}
-
+		// Upload failed, save the file locally
+		savelocalfile(file, filename);
 		alert('Saving file locally - failed to upload: ' + jqXHR.responseText);
 	      });
 	    }
@@ -111,13 +79,56 @@
 	};
 	// Opens the editor
 	window.addEventListener('message', receive);
-	source.drawIoWindow = window.open(url);
-      }
-      else
-      {
+	source.drawIoWindow = window.open(source.getAttribute('drawio-url'));
+      } else {
 	// Shows existing editor window
 	source.drawIoWindow.focus();
       }
     }
-  });
+  };
+
+  // Encode data as base64
+  function data2b64(data) {
+    var arr = new Uint8Array(data);
+    var raw = String.fromCharCode.apply(null, arr);
+    return (btoa(raw));
+  }
+
+  // Create blob from data URI
+  // https://stackoverflow.com/questions/4998908/convert-data-uri-to-file-then-append-to-formdata
+  function decodeURI(data) {
+    var byteString;
+    if (data.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(data.split(',')[1]);
+    else
+      byteString = unescape(data.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = data.split(',')[0].split(':')[1].split(';')[0];
+
+    // Create blob to hold data
+    var blob = new Blob([byteString], {type: mimeString});
+
+    return blob;
+  }
+
+  // Save a blob to a local file
+  // https://stackoverflow.com/questions/13405129/javascript-create-and-save-file
+  function savelocalfile(blob, filename) {
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+      var a = document.createElement("a"),
+          url = URL.createObjectURL(blob);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+	document.body.removeChild(a);
+	window.URL.revokeObjectURL(url);
+      }, 0);
+    }
+  }
+  document.addEventListener('dblclick', evthandler);
 })(jQuery);
